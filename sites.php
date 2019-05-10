d1cfbecb17de7fdca48e7681854d828d3f63230a
benmen1980 <benmen1@gmail.com>
2019-05-10 09:34:29 +0300

    Sites
    Add the sites to admin front end

---
+++ sites.php
@@ -0,0 +1,20 @@
+<?php defined('ABSPATH') or die('No direct script access!'); ?>
+
+<div class="wrap">
+
+    <?php include P18AW_ADMIN_DIR . 'header.php'; ?>
+
+    <div class="p18a-page">
+
+
+    <?php
+
+
+        $list = new PriorityWoocommerceAPI\Sites();
+        $list->prepare_items();
+        $list->display();
+
+    ?>
+    </div>
+
+</div>
