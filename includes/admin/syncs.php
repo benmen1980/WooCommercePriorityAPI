55817ba40fa46cfedeebe22ec2d63e8e3726fd90
benmen1980 <benmen1@gmail.com>
2019-05-10 09:51:32 +0300

    Sites

--- includes/admin/syncs.php
+++ includes/admin/syncs.php
@@ -205,6 +205,43 @@
                 </td>
             </tr>
 
+            <tr>
+                <td class="p18a-label">
+			        <?php _e('Sites Priority > Web', 'p18a'); ?>
+                </td>
+                <td>
+                    <input type="checkbox" name="log_sites_priority" form="p18aw-sync" value="1" <?php if($this->option('log_sites_priority')) echo 'checked'; ?> />
+                </td>
+                <td></td>
+                <td>
+                    <select name="auto_sync_sites_priority" form="p18aw-sync">
+                        <option value="" <?php if( ! $this->option('auto_sync_sites_priority')) echo 'selected'; ?>><?php _e('None', 'p18a'); ?></option>
+                        <option value="hourly" <?php if($this->option('auto_sync_sites_priority') == 'hourly') echo 'selected'; ?>><?php _e('Every hour', 'p18a'); ?></option>
+                        <option value="daily" <?php if($this->option('auto_sync_sites_priority') == 'daily') echo 'selected'; ?>><?php _e('Once a day', 'p18a'); ?></option>
+                        <option value="twicedaily" <?php if($this->option('auto_sync_sites_priority') == 'twicedaily') echo 'selected'; ?>><?php _e('Twice a day', 'p18a'); ?></option>
+                    </select>
+                </td>
+                <td data-sync-time="sync_sites_priority">
+			        <?php
+			        if ($timestamp = $this->option('sites_priority_update', false)) {
+				        echo(date('d/m/Y H:i:s', $timestamp));
+			        } else {
+				        _e('Never', 'p18a');
+			        }
+			        ?>
+                </td>
+                <td>
+                    <a href="#" class="button p18aw-sync" data-sync="sync_sites_priority"><?php _e('Sync', 'p18a'); ?></a>
+                </td>
+                <td>
+                    <input type="text" style="width:300px" name="email_error_sync_sites_priority" form="p18aw-sync"><?=$this->option('email_error_sync_sites_priority')?></input>
+                </td>
+                <td>
+                    <input type="text" style="width:300px" name="static_odata_header_sync_sites_priority" form="p18aw-sync"><?=$this->option('static_odata_header_sync_sites_priority')?></input>
+                </td>
+            </tr>
+
+
             <tr><td>_______________________________</td></tr>
             <!--  sync reciepts -->
             <tr>
