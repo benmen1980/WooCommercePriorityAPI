c8ecc18c2d358c243f4e5cc7d6d593420fa93b6e
benmen1980 <benmen1@gmail.com>
2020-04-02 12:07:43 +0300

    1.05

--- priority-woo-api.php
+++ priority-woo-api.php
@@ -8,7 +8,7 @@
 * Plugin Name: Priority Woocommerce API 
 * Plugin URI: http://www.roi-holdings.com
 * Description: Priority Woocommerce API extension
-* Version: 1.1
+* Version: 1.05
 * Author: Roi Holdings
 * Author URI: http://www.roi-holdings.com
 * Licence: GPLv2
