e93bbe5f3d48895cf4a7a460c03209f7baa41472
benmen1980 <benmen1@gmail.com>
2020-07-07 15:39:04 +0300

    clear old code and add disable after posting

--- includes/front/my-account/my-account.js
+++ includes/front/my-account/my-account.js
@@ -2,18 +2,8 @@ console.log('this is jquery for my account!');
 jQuery(document).ready(function($) {
     $('#simply-obligo').submit(function(e){
         e.preventDefault();
+        $('#obligoSubmit').attr('disabled', 'disabled');
         console.log('Open recipte by ajax!');
-   /* var data = {
-        'data' : jQuery("#simply-obligo").serialize(),
-        'action': 'my_action',
-        'whatever': 1234
-    };*/
-
-    // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
-        /*jQuery.post(my_ajax_object.ajaxurl, data, function(response) {
-
-            alert('Got this from the server: ' + response);
-        });*/
         data = $( this );
         jQuery.ajax({
             type:"POST",
