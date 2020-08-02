17e300583110063f8b27c6dcd177163378b9131d
benmen1980 <benmen1@gmail.com>
2020-08-02 22:01:33 +0300

    add the option to post OTC

--- includes/admin/syncs/sync_order.php
+++ includes/admin/syncs/sync_order.php
@@ -3,12 +3,27 @@
 defined('ABSPATH') or die('No direct script access!');
 
 if(isset($_GET['ord'])){
+	if($this->option('post_order_checkout')) {
+		echo '<h1>Priority API, sync Order to Priority</h1>';
+	}elseif($this->option('post_einvoices_checkout')) {
+		echo '<h1>Priority API, sync Over The Counter Invoice to Priority</h1>';
+	}
 	?>
-	<h1>Priority API, sync order to Priority</h1>
+
 	<?php
 	$order_id =  $_GET['ord'];
-	$response =  $this->syncOrder($order_id,'true');
-
+	// sync order
+	if($this->option('post_order_checkout')) {
+		$response = $this->syncOrder( $order_id );
+	}
+	// sync OTC
+	if($this->option('post_einvoices_checkout')) {
+		$response = $this->syncOverTheCounterInvoice( $order_id );
+	}
+	if($this->option('sync_onorder_receipts')) {
+		// sync receipts
+		$response = $this->syncReceipt($order_id);
+	}
 	echo '<h2>'.__('Request','woocommerce').'</h2>';
 	print_r($response['args']['body']);
 
