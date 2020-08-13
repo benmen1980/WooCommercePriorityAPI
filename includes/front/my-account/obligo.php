13c9be19ba9d7a351e632e437d172869f7983d44
benmen1980 <benmen1@gmail.com>
2020-08-14 02:03:14 +0300

    add the simply pay functionality

--- includes/front/my-account/obligo.php
+++ includes/front/my-account/obligo.php
@@ -26,8 +26,14 @@ class Obligo extends \PriorityAPI\API{
 	{
 		add_filter( 'woocommerce_get_item_data', [$this,'render_custom_data_on_cart_checkout'], 10, 2 );
 		add_filter( 'woocommerce_add_cart_item_data',[$this,'split_product_individual_cart_items'], 10, 2 );
+		if(isset($_GET['simplypay'])){
+			add_filter( 'wc_add_to_cart_message_html', [$this,'remove_add_to_cart_message']);
+        }
+		add_filter( 'woocommerce_add_cart_item_data',[$this,'simplypay'], 10, 2 );
 		add_action( 'woocommerce_before_calculate_totals', [$this,'add_custom_price']);
 		add_action( 'p18a_request_front_obligo',[$this,'request_front_obligo']);
+
+	//	add_action('woocommerce_before_checkout_form',[$this,'add_item_from_url']);
         // JS
 		add_action( 'wp_ajax_my_action',[$this,'my_action']);
 		add_action( 'wp_enqueue_scripts', [$this,'my_enqueue']);
@@ -88,6 +94,12 @@ class Obligo extends \PriorityAPI\API{
                   )
         );
 	}
+	public function add_item_from_url(){
+		$cart_item_data['_other_options']['product-price'] = 177.77 ;
+		$cart_item_data['_other_options']['product-ivnum'] = 'MY_IV000001' ;
+		$product_id = wc_get_product_id_by_sku('PAYMENT');
+		$cart           = WC()->cart->add_to_cart( $product_id, 1, null, null, $cart_item_data );
+    }
 	public function my_action() {
 
 		$data = $_POST['data'];
@@ -115,6 +127,24 @@ class Obligo extends \PriorityAPI\API{
 
 		wp_die(); // this is required to terminate immediately and return a proper response
 	}
+	// simply pay module
+    function simplypay(){
+	    if(isset($_GET['simplypay'])){
+		    $cart_item_data['_other_options']['product-price'] = $_GET['price'] ;
+		    $cart_item_data['_other_options']['product-ivnum'] = $_GET['ivnum'] ;
+		    WC()->session->set(
+			    'session_vars',
+			    array(
+				    'ordertype' => 'Recipe'));
+		    return $cart_item_data;
+	    }
+    }
+	function remove_add_to_cart_message( $message ){
+		return '';
+	}
+
+
+    // end any pay
 	function split_product_individual_cart_items( $cart_item_data, $product_id ){
 		if(isset($_POST['obligoSubmit'])){
 	    $unique_cart_item_key = uniqid();
