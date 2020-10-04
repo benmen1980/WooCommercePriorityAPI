2cb488d6d1f18526d6513babdfc7e56a525e5f1f
benmen1980 <benmen1@gmail.com>
2020-10-05 00:18:03 +0300

    add short code and session

--- includes/front/sites/sites.php
+++ includes/front/sites/sites.php
@@ -1,41 +1,130 @@
 <?php
-
-// actions
-if (\PriorityWoocommerceAPI\WooAPI::instance()->option( 'sites' ) == true ) {
-	add_filter('wp_nav_menu_items','add_site_to_menu', 1000, 1000);
-	add_action( 'wp_enqueue_scripts', 'wp2441_enqueue_scripts' );
+add_shortcode('priority-site','simply_populate_sites');
+add_action( 'wp_footer', 'simply_ajax_without_file' );
+/* handle session on frontend */
+function simply_ajax_without_file() { ?>
+    <script type="text/javascript" >
+        jQuery("#simply_sites").change(function($) {
+            ajaxurl = '<?php echo admin_url( 'admin-ajax.php' ) ?>'; // get ajaxurl
+            var data = {
+                'action': 'set_site_session', // your action name
+                'sitecode': jQuery("#simply_sites").val(),
+                'sitedesc': jQuery("#simply_sites option:selected").text()
+            }
+            jQuery.ajax({
+                url: ajaxurl, // this will point to admin-ajax.php
+                type: 'POST',
+                data: data,
+                success: function (response) {
+                    console.log(response);
+                }
+            });
+        });
+    </script>
+    <?php
 }
-// functions
-function wp2441_enqueue_scripts() {
+add_action("wp_ajax_set_site_session" , "set_site_session");
+add_action("wp_ajax_nopriv_set_site_session" , "set_site_session");
+function set_site_session(){
+    WC()->session->set( 'sitecode', $_POST['sitecode'] );
+    WC()->session->set( 'sitedesc', $_POST['sitedesc'] );
+    echo WC()->session->get( 'sitedesc');
+    wp_die();
+}
+// this is for populate sites in front not in check out
+function simply_populate_sites()
+{
+    // this function works with WC session
+    $option = "priority_customer_number";
+    $customer_number = get_user_option($option);
+    if(!empty(WC()->session->get( 'simply_sites'))){
+        $data = WC()->session->get( 'simply_sites');
+    }else {
+        $data = $GLOBALS['wpdb']->get_results('
+                            SELECT  sitecode,sitedesc
+                            FROM ' . $GLOBALS['wpdb']->prefix . 'p18a_sites
+                            where customer_number = ' . $customer_number,
+            ARRAY_A
+        );
+    }
+    if(empty($data)){
+        return __('No Sites','p18a');
+    }
+    WC()->session->set( 'simply_sites', $data);
+    // remove the ship to different address
+    add_filter( 'woocommerce_cart_needs_shipping_address', '__return_false');
+    $sitelist = array( // options for <select> or <input type="radio" />
+        '' => __('Please select', 'p18a'),
 
-	if ( !is_front_page() ){ // change for is_home() if you're not using a front page
-		wp_enqueue_script( 'frontSites', plugins_url( 'sites.js' , __FILE__ ), array('jquery') );
-		wp_localize_script( 'frontSites', 'site_ajax_page', array(
-			'url' => admin_url( 'admin-ajax.php' ),
-		));
-	}
+    );
+    $select_options = '<select name="sites" id="simply_sites">';
+    $selected_code = WC()->session->get('sitecode');
+    foreach ($data as $site) {
+        if ($selected_code == $site['sitecode']) {
+            $selected = ' selected';
+        }
+        $select_options .= '<option value="' . $site['sitecode'] . '"' . $selected . '>' . $site['sitedesc'] . '</option>';
+        $selected = '';
+    }
+    $select_options .= '</select>';
+    $items =  '<li class="menu-item">'
+        . '<p>'.$select_options.'</p>'
+        . '</li>';
+    return $items;
+}
+
+// add the site to order meta
+add_action('woocommerce_checkout_create_order', 'simply_before_checkout_create_order', 20, 2);
+function simply_before_checkout_create_order( $order, $data ) {
+    $order->update_meta_data( 'priority_site_code', WC()->session->get( 'sitedesc') );
+}
+
+
+// add check out, currently not in use
+//add_action( 'woocommerce_after_checkout_billing_form', array( $this ,'custom_checkout_fields'));
+/*
+function custom_checkout_fields( $checkout ){
+
+    //  add site to check out form
+    if($this->option('sites') == true) {
+        $option          = "priority_customer_number";
+        $customer_number = get_user_option( $option );
+        $data            = $GLOBALS['wpdb']->get_results( '
+            SELECT  sitecode,sitedesc
+            FROM ' . $GLOBALS['wpdb']->prefix . 'p18a_sites
+            where customer_number = ' . $customer_number,
+            ARRAY_A
+        );
+
+        $sitelist = array( // options for <select> or <input type="radio" />
+            '' => __('Please select','p18a'),
+
+        );
+
+        $finalsites = $sitelist;
+        foreach ( $data as $site ) {
+            $finalsites +=  [$site['sitecode'] => str_replace('"', '', $site['sitedesc'])];
+        }
+        //$i = 0;
+        //$site = array($data[$i]['sitecode'] => $data[$i]['sitedesc']);
+
+        $sites = array(
+            'type'        => 'select',
+            // text, textarea, select, radio, checkbox, password, about custom validation a little later
+            'required'    => true,
+            // actually this parameter just adds "*" to the field
+            'class'       => array( 'misha-field', 'form-row-wide' ),
+            // array only, read more about classes and styling in the previous step
+            'label'       => __('Priority ERP Order site ','p18a'),
+            'label_class' => 'misha-label',
+            // sometimes you need to customize labels, both string and arrays are supported
+            'options'     => $finalsites
+        );
+        woocommerce_form_field( 'site', $sites, $checkout->get_value( 'site' ) );
+    }
+
+
 
 }
-//  add site to check out form
-function add_site_to_menu($items, $args) {
-		$option          = "priority_customer_number";
-		$customer_number = get_user_option( $option );
-		$data            = $GLOBALS['wpdb']->get_results( '
-                SELECT  sitecode,sitedesc
-                FROM ' . $GLOBALS['wpdb']->prefix . 'p18a_sites
-                where customer_number = ' . $customer_number,
-			ARRAY_A
-		);
-		$selectopen = '<select name="sites" id="p18a-sites">';
-		$options = '';
-		foreach ($data as $site){
-			$sitedes = $site['sitedesc'];
-			$sitecode = $site['sitecode'];
-			$options .= ' <option value="'.$sitecode.'">'.$sitedes.'</option>';
-		}
-		$selectclose = '</select>';
-		$items .=  '<li class="menu-item">'
-		           . '<p>'.$selectopen.$options.$selectclose.'</p>'
-		           . '</li>';
-	return $items;
-}
+*/
+
