f079a5c18224fe3442f94c0cb8001ee77bbc9817
benmen1980 <benmen1@gmail.com>
2020-10-04 17:07:52 +0300

    fix to feet cart and product-category

--- assets/packs.js
+++ assets/packs.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function( $ ) {
-    $('#pri-packs').on('change', function() {
-        $('.qty').first().attr('step',this.value);
-        $('.qty').first().val(0);
-
+    $('.pri-packs').on('change', function(event ) {
+        // shop- wait for Sunli to fix his bug T20
+        // cart and product
+        $(event.target).parent().find('.input-text.qty.text').first().attr('step',this.value);
+        $(event.target).parent().find('.input-text.qty.text').first().val(0);
     });
-
 });
