a7df2f48327c589a57b06cb6e9599afbbe3c1098
benmen1980 <benmen1@gmail.com>
2020-10-04 17:05:08 +0300

    add class pri-packs to elements select

--- flatsome-child/woocommerce/cart/cart.php
+++ flatsome-child/woocommerce/cart/cart.php
@@ -15,6 +15,7 @@
  * @version 3.8.0
  */
 
+
 defined( 'ABSPATH' ) || exit;
 
 $row_classes     = array();
@@ -45,12 +46,218 @@ do_action( 'woocommerce_before_cart' ); ?>
 <div class="cart-wrapper sm-touch-scroll">
 
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
+	
+	<?php 
+	if(isMobileDevice()){
+	?>
+  	<div class="row row-main">
+            <div class="large-12 col">
+                <div class="col-inner">
+                    <div class="woocommerce">
+                    	<?php
+                    	$i=0;
+                    	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+							$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+							$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
+							if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+									$product_permalink = apply_filters( 'woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '', $cart_item, $cart_item_key );
+						?>
+                        <div class="cart_items_wrapper woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+                            <div class="cart_product">
+                                <div class="p-img product-thumbnail">
+                                   <?php
+									$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
+
+									if ( ! $product_permalink ) {
+										echo $thumbnail; // PHPCS: XSS ok.
+									} else {
+										printf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $thumbnail ); // PHPCS: XSS ok.
+									}
+									?>
+                                </div>
+                                <div class="p-details">
+                                    <h4 class="p-name">
+                                    	<?php
+										if ( ! $product_permalink ) {
+											echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
+										} else {
+											echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
+										}
+
+										do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+
+										// Meta data.
+										echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+
+										// Backorder notification.
+										if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+											echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'woocommerce' ) . '</p>', $product_id ) );
+										}
+										?>
+                                    </h4> 
+                                    <p class="mkt">SKU 4116</p>
+                                    <p class="p-price">
+                                    	<?php esc_attr_e( 'Total Quantity', 'woocommerce' ); ?> <span> 2 | <?php echo $cart_item['quantity']; ?></span>
+                                    </p>
+                                    <p class="p-price">
+                                    	<?php 
+                                    	esc_attr_e('Price','woocommerce'); ?><span> 2 | <?php echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key ); ?></span>
+                                    </p>
+                                    <p class="p-price">
+                                    	<?php 
+                                    	esc_attr_e('Total Price','woocommerce');
+                                    	?>
+                                    	<span> 
+                                    		<?php
+											$priceaftervat = number_format(($cart_item['line_subtotal'] + $cart_item['line_subtotal_tax']), '2', '.','');
+											echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'. get_woocommerce_currency_symbol()  . '</span>'.$priceaftervat.'</span>';
+											?> | <?php
+											echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
+												?>
+											
+										</span>
+                                    </p>
+                                   
+                                    
+                                </div>
+                                <div class="p-action">
+                                    
+                                    	<?php 
+                                    	echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+										'woocommerce_cart_item_remove_link',
+										sprintf(
+											'<a href="%s" class="remove delete-icon" aria-label="%s" data-product_id="%s" data-product_sku="%s"><img src="'.get_stylesheet_directory_uri().'/images/trash.png"></a>',
+											esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+											esc_html__( 'Remove this item', 'woocommerce' ),
+											esc_attr( $product_id ),
+											esc_attr( $_product->get_sku() )
+										),
+										$cart_item_key
+										);
+										?>
+                                    
+                                    <a href="javascript:void(0);" class="edit-icon" data-uid="<?php echo $i; ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/edit.png"></a>
+                                </div>
+                            </div>
+
+                            <div class="c_edit_detail" id="edit_details_<?php echo $i; ?>">
+                               <div class="p-pack">
+                                  <div class="qty"><?php esc_attr_e( 'Total Quantity :', 'woocommerce' ); ?> <span> <?php echo $cart_item['quantity']; ?></span></div>
+                                  <div class="select-pack">
+                                  	<?php
+									$product = wc_get_product($cart_item['product_id']);
+									$id = $cart_item['product_id'];
+									$packs = get_post_meta($id, 'packs', true);
+									?>
+									<div class="pack-wrapper">
+										<?php
+										if (!empty($packs)) {
+											?>
+											<label for="pri-packs"><?php _e('Choose a pack:', 'storefront');?></label>
+
+											<select name="packs" id="pri-packs" class="pri-packs">
+
+											<?php
+											foreach ($packs as $pack) {
+												echo $pack['PACKNAME'] . ' ' . $pack['PACKQUANT'] . '<br>';
+												echo ' <option value="' . $pack['PACKQUANT'] . '">' . $pack['PACKNAME'] . '</option>';
+											}
+											?>
+											</select>
+											<?php
+										}
+										
+									?>
+                                  </div>
+                               </div>
+                               <div class="quantity-pack">
+                               	<?php 
+                               	if (!$product->is_sold_individually() && $product->is_purchasable()) {
+											woocommerce_quantity_input(array('input_value' => $packs[0]['PACKQUANT'], 'min_value' => 0)); 
+										}else{
+											sprintf( '1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key );
+										}
+                               	?>
+                               </div>
+                           	</div>
+                               <div class="unit-price">
+                               	<?php
+                                	esc_attr_e('Unit Price:','woocommerce');
+                                echo apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );  ?></span>
+                               </div>
+                               <div class="discount-price">
+                               		<?php 
+                               		$discount_price = number_format(($cart_item['line_total'] - $cart_item['line_subtotal']),'2','.','') ;
+							
+                               		?>
+                                    <div> <?php 
+                                    esc_attr_e('Price incl. Discount: ','woocommerce');
+                                    	$p_price = $cart_item['data']->get_price();
+										$finalprice = number_format(($p_price - $discount_price),'2','.','') ;
+								
+										echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'. get_woocommerce_currency_symbol()  . '</span>'.$finalprice.'</span>';?>
+                                    </div>
+                                    <div class="discount">
+                                    	<?php 
+                                    	
+										echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'. get_woocommerce_currency_symbol()  . '</span>'.$discount_price.'</span>'; ?>
+									</div>
+                               </div>
+                               <div class="total-vat">
+                                    <div>
+                                    	<?php
+                                    	esc_attr_e('Total incl. VAT :','woocommerce');
+                                    	$priceaftervat = number_format(($cart_item['line_subtotal'] + $cart_item['line_subtotal_tax']), '2', '.','');
+										echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'. get_woocommerce_currency_symbol()  . '</span>'.$priceaftervat.'</span>';
+                                    	?>
+                                    </div>
+                                    <div class="total-price">
+                                    	<?php
+                                    	esc_attr_e('Total :','woocommmerce');
+                                    	echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); 
+										
+										?>
+                                    </div>
+                               </div>
+ 
+                               <div class="commentbox">
+                               		<?php 
+									$notes = isset( $cart_item['notes'] ) ? $cart_item['notes'] : '';
+									 printf(
+									 '<div><input placeholder="Add comment" type="text" class="%s" id="cart_notes_%s" data-cart-id="%s" value="%s"></div>','winn_cart_notes',
+									 $cart_item_key,
+									 $cart_item_key,
+									 $notes
+									 );
+									 ?>
+                               </div>
+                            </div>
+
+                        </div>
+                        <?php 
+                    	}
+                    	$i++;
+                    } //foreach over
+                        ?>
+                    </div>
+                </div> 
+            </div>
+            <div class="large-12 col">
+            	<button type="submit" class="button primary mt-0 pull-left small" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>"><?php esc_html_e( 'Update cart', 'woocommerce' ); ?></button>
+
+					<?php fl_woocommerce_version_check( '3.4.0' ) ? wp_nonce_field( 'woocommerce-cart', 'woocommerce-cart-nonce' ) : wp_nonce_field( 'woocommerce-cart' ); ?>
+			</div>
+            
+        </div>
+  	<?php
+	}else {
+	?>
 	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 		<thead>
 			<tr>
 				<th class="product-name" colspan="3"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-				<th class="product-packs"><?php esc_html_e( 'Packs', 'woocommerce' ); ?></th>
+				<th class="product-packs"><?php esc_html_e( 'Packs', 'flatsome' ); ?></th>
 				<th class="product-quantity"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
 				<th class="product-price"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
 				<th class="product-discount"><?php esc_html_e( 'Discount', 'woocommerce' ); ?></th>
@@ -73,7 +280,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 					?>
 					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
-						<td class="product-remove">
+						<td rowspan="2" class="product-remove">
 							<?php
 								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 									'woocommerce_cart_item_remove_link',
@@ -89,7 +296,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 						</td>
 
-						<td class="product-thumbnail">
+						<td rowspan="2" class="product-thumbnail">
 						<?php
 						$thumbnail = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 
@@ -101,7 +308,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 						?>
 						</td>
 
-						<td class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
+						<td rowspan="2" class="product-name" data-title="<?php esc_attr_e( 'Product', 'woocommerce' ); ?>">
 						<?php
 						if ( ! $product_permalink ) {
 							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
@@ -128,7 +335,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 								?>
 							</div>
 						</td>
-						<td class="product-packs" data-title="<?php esc_attr_e('Packs','woocommerce'); ?>">
+						<td rowspan="2" class="product-packs" data-title="<?php esc_attr_e('Packs','woocommerce'); ?>">
 							<?php
 							$product = wc_get_product($cart_item['product_id']);
 							$id = $cart_item['product_id'];
@@ -140,7 +347,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 									?>
 									<label for="pri-packs"><?php _e('Choose a pack:', 'storefront');?></label>
 
-									<select name="packs" id="pri-packs">
+									<select name="packs" id="pri-packs" class="pri-packs">
 
 									<?php
 									foreach ($packs as $pack) {
@@ -212,10 +419,14 @@ do_action( 'woocommerce_before_cart' ); ?>
 						</td>
 						<td class="product-totalaftervat" data-title="<?php esc_attr_e( 'Total After VAT', 'woocommerce' ); ?>">
 							<?php
+
 								$priceaftervat = number_format(($cart_item['line_subtotal'] + $cart_item['line_subtotal_tax']), '2', '.','');
 								echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'. get_woocommerce_currency_symbol()  . '</span>'.$priceaftervat.'</span>';
 							?>
 						</td>
+						
+					</tr>
+					<tr>
 						<td colspan="8"  class="comment-line" data-title="<?php esc_attr_e('Comment line','woocommerce'); ?>">
 						
 							<?php 
@@ -230,7 +441,6 @@ do_action( 'woocommerce_before_cart' ); ?>
 						
 						</td>
 					</tr>
-					
 					<?php
 				}
 			}
@@ -252,6 +462,9 @@ do_action( 'woocommerce_before_cart' ); ?>
 			<?php do_action( 'woocommerce_after_cart_contents' ); ?>
 		</tbody>
 	</table>
+	<?php 
+	} //desktop over
+	?>
 	<?php do_action( 'woocommerce_after_cart_table' ); ?>
 </div>
 </form>
@@ -295,3 +508,13 @@ do_action( 'woocommerce_before_cart' ); ?>
 <?php do_action( 'woocommerce_after_cart' ); ?>
 
 <script type="text/javascript" src="<?php echo P18AW_ASSET_URL . 'packs.js';?>"></script>
+<script>
+	jQuery(document).ready(function(){
+		jQuery('body').on('click','.edit-icon',function(){
+			jQuery(this).toggleClass('active');
+			var uniqueid = jQuery(this).attr('data-uid');
+			jQuery('#edit_details_'+uniqueid).toggle();
+			//jQuery(this).parents('.cart_items_wrapper').siblings('.c_edit_detail').css('display','flex');
+		});
+	});
+</script>
