170d5a339f524eb6e99ea2bf3adf21d38edff186
Roy <benmen1@gmail.com>
2020-10-25 10:29:47 +0200

    fix the repost functionality

--- includes/admin/syncs/sync_order.php
+++ includes/admin/syncs/sync_order.php
@@ -2,60 +2,54 @@
 
 defined('ABSPATH') or die('No direct script access!');
 
+function simply_create_message_repost($response){
+	$message  = '<h2>Request</h2>';
+	$message .= $response['args']['body'].'<br>';
+	$message .= '<h2>Response</h2>';
+	$message .= $response['body'].'<br>';
+	return $message;
+}
+
 if(isset($_GET['ord'])){
+	/*
 	if($this->option('post_order_checkout')) {
 		echo '<h1>Priority API, sync Order to Priority</h1>';
 	}elseif($this->option('post_einvoices_checkout')) {
 		echo '<h1>Priority API, sync Over The Counter Invoice to Priority</h1>';
 	}
+	*/
 	?>
-
 	<?php
 	$order_id =  $_GET['ord'];
+	$message = '';
 	// sync order
 	if($this->option('post_order_checkout')) {
-		$response_order = $this->syncOrder( $order_id );
+		$message .= '<h1>Priority API, sync Order to Priority</h1>';
+		$response = $this->syncOrder( $order_id );
+		$message .= simply_create_message_repost($response);
 	}
 	// sync OTC
 	if($this->option('post_einvoices_checkout')) {
-		$response_otc = $this->syncOverTheCounterInvoice( $order_id );
+		$message .= '<h1>Priority API, sync Over The Counter Invoice to Priority</h1>';
+		$response = $this->syncOverTheCounterInvoice( $order_id );
+		$message .= simply_create_message_repost($response);
 	}
 	// sync ainvoices
-	if($this->option('sync_onorder_ainvoices')) {
-		$response_aiv = $this->syncAinvoice($order_id);
+	if($this->option('post_ainvoice_checkout')) {
+		$message .= '<h1>Priority API, sync Sales Invoice to Priority</h1>';
+		$response = $this->syncAinvoice($order_id);
+		$message .= simply_create_message_repost($response);
 	}
 	// sync reciepe
-	if($this->option('sync_onorder_receipts')) {
-		$response_rec = $this->syncReceipt($order_id);
+	if($this->option('post_receipt_checkout')) {
+		$message .= '<h1>Priority API, sync Receipt to Priority</h1>';
+		$response = $this->syncReceipt($order_id);
+		$message .= simply_create_message_repost($response);
 	}
-
-	echo '<h2>'.__('Request','woocommerce').'</h2>';
-	print_r($response_order['args']['body']);
-	echo '<br><br>';
-	print_r($response_otc['args']['body']);
-	echo '<br><br>';
-	print_r($response_aiv['args']['body']);
-	echo '<br><br>';
-	print_r($response_rec['args']['body']);
-
-
-
-	echo '<h2>'.__('Response','woocommerce').'</h2>';
-	print_r($response_order['body']);
-	echo '<br><br>';
-	print_r($response_otc['body']);
-	echo '<br><br>';
-	print_r($response_aiv['body']);
-	echo '<br><br>';
-	print_r($response_rec['body']);
-	echo '<br><br>';
-
+	// message
+	echo $message;
 	echo '<br><br><br><h2>'.__('Refresh this page to re post again','woocommerce').'</h2>';
-
 	echo '<br><br><br><a href="edit.php?post_type=shop_order">'.__('Back to order list','woocommerce').'</a>';
-
-
-
 }else {
 	wp_die('You got her by mistake ?');
 }
