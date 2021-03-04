d06b938f4fff51275cfeaec519e1997ff7b73363
dc.narola <dc@narola.email>
2021-03-04 17:17:04 +0530

    Minor JS change in frontend.js

--- assets/frontend.js
+++ assets/frontend.js
@@ -19,18 +19,20 @@ jQuery(document).ready(function($) {
 
 	// jQuery('.toggle').click(function(){
 	jQuery(document).on('click','.cust-toggle', function() {
-
 		console.log('clicked');
 		var cls=jQuery(this).attr('id');
 		if(jQuery('.subform-'+cls).hasClass('active')){
 			jQuery(this).text('+').addClass('plus').removeClass('minus');
-			jQuery('.subform-'+cls).hide();	
+			//jQuery('.subform-'+cls).hide();
+			jQuery('.subform-'+cls).css('display','none');	
 			jQuery('.subform-'+cls).removeClass('active');
 		}else{
 			jQuery('.cust-toggle').text('+').addClass('plus').removeClass('minus');
 			jQuery(this).text('-').addClass('minus').removeClass('plus');
-			jQuery('.content_value').hide().removeClass('active');	
-			jQuery('.subform-'+cls).show();
+			//jQuery('.content_value').hide().removeClass('active');
+			jQuery('.content_value').css('display','none').removeClass('active');	
+			//jQuery('.subform-'+cls).show();
+			jQuery('.subform-'+cls).css('display','table-row');
 			jQuery('.subform-'+cls).addClass('active');
 
 
