4a460e0ba9cdb15eb73cc8cbec5a535de244b9e0
elicheva katz <elisheva.g@simplyct.co.il>
2021-04-25 12:23:27 +0300

    add sum of all checked row in obligo

--- includes/front/my-account/obligo.php
+++ includes/front/my-account/obligo.php
@@ -225,8 +225,13 @@ class Obligo extends \PriorityAPI\API{
 		echo "</table>";
 		echo "<form id='simply-obligo' action='' method='post'>";
 
-		echo '<input type="hidden" name="action" value="my_action_obligo" />';
-		echo "<button type='submit' name='obligoSubmit' id='obligoSubmit' style='float: right;' disabled>".__('Pay now','p18w')."</button>";
+		echo '<input type="hidden" name="action" value="my_action_obligo" />';?>
+		<p>
+			<?php echo __('Total Payment: ','p18w') ;?>
+			<span class="total_payment_checked">0</span>
+			<span><?php echo get_woocommerce_currency_symbol(); ?></span>
+		</p>
+		<?php echo "<button type='submit' name='obligoSubmit' id='obligoSubmit' style='float: right;' disabled>".__('Pay now','p18w')."</button>";
 
 		echo "<table> <tr>";
 		echo "<th></th><th>".esc_html__('BALDATE','p18w')."</th> <th>".esc_html__('FNCNUM','p18w')."</th> <th>".esc_html__('IVNUM','p18w')."</th> <th>".esc_html__('DETAILS','p18w')."</th> <th>".esc_html__('SUM1','p18w')."</th>";
