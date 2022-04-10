b64b822b2750fb5ea3fbe8b7c973c6d3e5654bbc
Ruth-Z-SimpleCT <ruth.z@roi-holdings.com>
2022-04-10 14:13:30 +0300

    Packs

--- includes/admin/packs.php
+++ includes/admin/packs.php
@@ -7,7 +7,7 @@ function woo_add_custom_general_fields()
     // You can create text, textarea, select, checkbox and custom fields
 
     global $woocommerce, $post;
-    $packs = get_post_meta($post->ID, 'pri_packs', true);
+    $packs = get_post_meta($post->ID, 'pri_packs',false);
     if (!empty($packs)) {
         echo '<div class="options_group">';
 
@@ -18,7 +18,7 @@ function woo_add_custom_general_fields()
             <span class="wrap">
         <?php
 
-        foreach ($packs as $pack) {
+        foreach ($packs[0] as $pack) {
             echo $pack['PACKNAME'] . ' ' . $pack['PACKQUANT'] . '<br>';
         }
 
@@ -33,51 +33,22 @@ function woo_add_custom_general_fields()
 
 }
 
-/*
-add_filter( 'woocommerce_quantity_input_args', 'bloomer_woocommerce_quantity_changes', 10, 2 );
 
-function bloomer_woocommerce_quantity_changes( $args, $product ) {
-
-	if ( ! is_cart() ) {
-		global $product;
-		$id    = $product->get_id();
-		$packs = get_post_meta( $id, 'packs', true );
-		if ( !empty( $packs ) ) {
-			$args['input_value'] = $packs[0]['PACKQUANT']; // Start from this value (default = 1)
-			$args['max_value']   = 1000; // Max quantity (default = -1)
-			$args['min_value']   = $packs[0]['PACKQUANT']; // Min quantity (default = 0)
-			$args['step']        = $packs[0]['PACKQUANT']; // Increment/decrement by this value (default = 1)
-		}
-
-	} else {
-
-		// Cart's "min_value" is already 0 and we don't need "input_value"
-		$args['max_value'] = 10; // Max quantity (default = -1)
-		$args['step'] = 2; // Increment/decrement by this value (default = 0)
-		// COMMENT OUT FOLLOWING IF STEP < MIN_VALUE
-		// $args['min_value'] = 4; // Min quantity (default = 0)
-
-	}
-
-	return $args;
-
-}
-*/
 // define the woocommerce_before_add_to_cart_button callback
 function action_woocommerce_before_add_to_cart_button()
 {
     global $product;
     $id = $product->get_id();
-    $packs = get_post_meta($id, 'pri_packs', true);
+    $packs = get_post_meta($id, 'pri_packs', false);
     if (!empty($packs)) {
         ?>
         <label for="cars"><?php _e('Choose a pack:', 'storefront'); ?></label>
 
         <select name="packs" id="pri-packs">
             <?php
-            foreach ($packs as $pack) {
+            foreach ($packs[0] as $pack) {
                 echo $pack['PACKNAME'] . ' ' . $pack['PACKQUANT'] . '<br>';
-                echo ' <option value="' . $pack['PACKQUANT'] . '">' . $pack['PACKNAME'] . '</option>';
+                echo ' <option  value="' . $pack['PACKQUANT'] . '" pack-code="'.$pack['PACKCODE'].'">' . $pack['PACKNAME'].' | '.$pack['PACKQUANT'] . '</option>';
             }
             ?>
         </select>
@@ -90,7 +61,7 @@ function action_woocommerce_before_add_to_cart_button()
 // add the action product page
 add_action('woocommerce_before_add_to_cart_form', 'action_woocommerce_before_add_to_cart_button', 10, 0);
 //  add the action shop page
-//add_action( 'woocommerce_shop_loop_item_title', 'action_woocommerce_before_add_to_cart_button' );
+add_action( 'woocommerce_shop_loop_item_title', 'action_woocommerce_before_add_to_cart_button' );
 
 add_action('wp_enqueue_scripts', 'my_theme_scripts');
 function my_theme_scripts()
@@ -103,7 +74,7 @@ function my_theme_scripts()
 add_filter('woocommerce_quantity_input_args', 'jk_woocommerce_quantity_input_args', 10, 2); // Simple products
 function jk_woocommerce_quantity_input_args($args, $product)
 {
-    $packs = get_post_meta($product->get_id(), 'pri_packs', true);
+    $packs = get_post_meta($product->get_id(), 'pri_packs', false);
     if (empty($packs)) {
         return $args;
     }
