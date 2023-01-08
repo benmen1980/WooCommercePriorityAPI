2ae34d396b59c2de7e28c1fb42d2ae49f604b519
unknown <benmen1980@gmail.com>
2023-01-08 18:13:28 +0200

    sync inventory add expand to filter and add code to zikit

--- includes/classes/wooapi.php
+++ includes/classes/wooapi.php
@@ -2453,7 +2453,7 @@ class WooAPI extends \PriorityAPI\API
         }
         $option_filed = explode(',', $this->option('sync_inventory_warhsname'))[2];
         $data['select'] = (!empty($option_filed) ? $option_filed . ',PARTNAME' : 'PARTNAME');
-        $data = apply_filters('simply_syncInventoryPriority_data', $data);
+
         $wh_name = explode(',', $this->option('sync_inventory_warhsname'))[0];
         $status = explode(',', $this->option('sync_inventory_warhsname'))[4];
         if (!empty($wh_name)) {
@@ -2468,7 +2468,9 @@ class WooAPI extends \PriorityAPI\API
         } else {
             $expand = '$expand=LOGCOUNTERS_SUBFORM,PARTBALANCE_SUBFORM';
         }
-        $response = $this->makeRequest('GET', 'LOGPART?$select='.$data['select'].'&$filter='.$url_addition.' and INVFLAG eq \'Y\' &' . $expand, [], $this->option('log_inventory_priority', false));
+        $data['expand'] = $expand;
+	    $data = apply_filters('simply_syncInventoryPriority_data', $data);
+        $response = $this->makeRequest('GET', 'LOGPART?$select='.$data['select'].'&$filter='.$url_addition.' and INVFLAG eq \'Y\' &' . $data['expand'], [], $this->option('log_inventory_priority', false));
         // check response status        // check response status
         if ($response['status']) {
             $data = json_decode($response['body_raw'], true);
--- priority-woo-api.php
+++ priority-woo-api.php
@@ -8,7 +8,7 @@
  * Plugin Name: Priority Woocommerce API
  * Plugin URI: http://simplyCT.co.il
  * Description: Priority Woocommerce API extension
- * Version: 1.50.5
+ * Version: 1.50.6
  * Author: SimplyCT
  * Author URI: http://www.simplyCT.co.il
  * Licence: GPLv2
