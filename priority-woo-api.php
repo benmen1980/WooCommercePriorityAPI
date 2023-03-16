50a4066a37d3768ff30c3210caf35591c69be261
unknown <benmen1980@gmail.com>
2023-03-16 17:06:05 +0200

    add filter to variation sync

--- priority-woo-api.php
+++ priority-woo-api.php
@@ -8,7 +8,7 @@
  * Plugin Name: Priority Woocommerce API
  * Plugin URI: http://simplyCT.co.il
  * Description: Priority Woocommerce API extension
- * Version: 1.53.5
+ * Version: 1.53.6
  * Author: SimplyCT
  * Author URI: http://www.simplyCT.co.il
  * Licence: GPLv2
