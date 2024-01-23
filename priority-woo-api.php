4b20d9850f551741c951bdd2b297a937190b957e
esterNachmani <ester.n@roi-holdings.com>
2024-01-23 14:38:16 +0200

    update version

--- priority-woo-api.php
+++ priority-woo-api.php
@@ -8,7 +8,7 @@
  * Plugin Name: Priority Woocommerce API
  * Plugin URI: http://simplyCT.co.il
  * Description: Priority Woocommerce API extension
- * Version: 2.4.8
+ * Version: 2.4.9
  * Author: SimplyCT
  * Author URI: http://www.simplyCT.co.il
  * Licence: GPLv2
